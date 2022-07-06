8ce48b85d7ca6267950c12677d7c95c5ebd6942f
kurosu31 <nezimakidance@gmail.com>
2022-07-06 19:58:43 +0900

    サービスコンテナ

---
+++ laravel_breeze/app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    //
+
+    public function showServiceContainerTest()
+    {
+        app()->bind('lifeCycleTest', function () {
+            return "ライフサイクルテスト";
+        });
+        // サービスコンテナから取り出す。
+        $test = app()->make('lifeCycleTest');
+        // 他の書き方
+        // $test = app('lifeCycleTest');
+        // $test = resolve('lifeCycleTest');
+        // $test =App::make('lifeCycleTest');
+
+        // サービスコンテナなしのパターン
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+        // サービスコンテナapp()ありのパターン
+        app()->bind('sample', Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
+
+        dd($test, app());
+    }
+}
+
+class Sample
+{
+    public $message;
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+    public function run()
+    {
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send()
+    {
+        echo ('メッセージ表示');
+    }
+}
