e01f88579b7ab8c88eaf232d0de68edecea34c7a
kurosu31 <nezimakidance@gmail.com>
2022-07-15 10:18:39 +0900

    アプリ名、ロゴ

--- laravel_breeze/resources/views/admin/dashboard.blade.php
+++ laravel_breeze/resources/views/admin/dashboard.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <p>admin</p>
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
